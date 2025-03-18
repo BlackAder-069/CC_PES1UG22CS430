6a5629c0da3c5695e9e75025af435df06453c4c7
Pranav Jigalur <pranav.jigalur@gmail.com>
2025-03-18 16:24:01 +0530

    Update hello.cpp by pes1ug22cs430

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<< "My Name is Pranav Jigalur \n";
+  cout<< "My Name is Pranav Jigalur\n";
   cout << "My SRN is PES1UG22CS430 \n";
   return 0;
 }
